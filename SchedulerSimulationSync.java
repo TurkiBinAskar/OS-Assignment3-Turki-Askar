3d158aaaca91cd39b5b771a5d4a8753ecaba29b8
TurkiBinAskar <445050245@std.psau.edu.sa>
2026-05-02 23:11:09 +0300

    Implement locks and semaphore

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -38,34 +38,59 @@ class SharedResources {
     
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
+    public static final ReentrantLock contextSwitchLock = new ReentrantLock();
+    public static final ReentrantLock completedProcessLock = new ReentrantLock();
+    public static final ReentrantLock totalWaitingTimeLock = new ReentrantLock();
+    public static final ReentrantLock executionLogLock = new ReentrantLock();
     
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
+    public static final Semaphore cpuSemaphore = new Semaphore(1);
     
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+       contextSwitchLock.lock(); 
+        try {
+            contextSwitchCount++;
+        } finally {
+            contextSwitchLock.unlock(); 
+        }
     }
     
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+        completedProcessLock.lock();
+        try {
+            completedProcessCount++;
+        } finally {
+            completedProcessLock.unlock();
+        }
     }
     
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+       totalWaitingTimeLock.lock();
+        try {
+            totalWaitingTime += time;
+        } finally {
+            totalWaitingTimeLock.unlock();
+        }
     }
     
     // Method to log execution
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        executionLogLock.lock();
+        try {
+            executionLog.add(message);
+        } finally {
+            executionLogLock.unlock();
+        }
     }
 }
 
